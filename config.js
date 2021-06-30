e4eb62b9d01a6eea82dfae2b497eaef67295a245
Innan Yun <innan.yun@gmail.com>
2021-06-30 10:23:28 +0800

    externalized scene init parameters

---
+++ config.js
@@ -0,0 +1,12 @@
+export const
+
+  SHEET_OPTIONS = {
+    color: 0x00ff00
+  },
+
+  RENDER_OPTIONS = {
+    backgroundColor: '#ceedce',
+    fov: 75,    // field of view
+    near: 0.1,  // near plane of viewing frustum
+    far: 1000   // far plane of viewing frustum
+  }
