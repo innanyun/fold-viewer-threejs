839a2ebdad2fae80f250aa611798644cbeaedff0
Innan Yun <innan.yun@gmail.com>
2021-06-30 12:25:48 +0800

    split modules by functions

--- sheet.js
+++ sheet.js
@@ -1,6 +1,4 @@
 import * as THREE from 'three';
-import * as dat from 'dat.gui'
-import { gsap } from 'gsap'
 
 
 //
@@ -8,7 +6,7 @@ import { gsap } from 'gsap'
 //
 
 
-function createSheetGeometry () {
+export function createSheetGeometry () {
   const vertices = [
     [-0.5, -0.5],
     [ 0.5, -0.5],
@@ -22,123 +20,3 @@ function createSheetGeometry () {
     )
   );
 }
-
-
-//
-// Three.JS stuff
-//
-
-
-let scene, camera, renderer, mesh;
-
-
-function initScene (renderOptions) {
-  scene = new THREE.Scene();
-
-  camera = new THREE.PerspectiveCamera(
-    renderOptions.fov,  // field of view
-    window.innerWidth / window.innerHeight,  // aspect ratio
-    renderOptions.near,  // near plane of viewing frustum
-    renderOptions.far    // far plane of viewing frustum
-  );
-  camera.position.z = 5;
-
-  const
-    geometry = createSheetGeometry(),
-    material = new THREE.MeshStandardMaterial(
-      {color: SHEET_OPTIONS.color, side: THREE.DoubleSide}
-    );
-
-  mesh = new THREE.Mesh(geometry, material);
-
-  let light = new THREE.PointLight(0xffffff);
-  light.position.set(-10, 40, 10);
-
-  scene.background = new THREE.Color(renderOptions.backgroundColor)
-  scene.add(mesh);
-  scene.add(light);
-
-  scene.add(new THREE.AxesHelper())
-
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-}
-
-
-function animateScene () {
-  renderer.render(scene, camera);
-
-  window.requestAnimationFrame(animateScene);
-}
-
-
-//
-// debug UI
-//
-
-
-const
-  SHEET_OPTIONS = {
-    color: 0x00ff00
-  },
-  RENDER_OPTIONS = {
-    backgroundColor: '#ceedce',
-    fov: 75,  // field of view
-    near: 0.1,  // near plane of viewing frustum
-    far: 1000  // far plane of viewing frustum
-  }
-
-
-function initDatGUI (mesh, camera) {
-  const
-    gui = new dat.GUI({name: 'JobKey II'}),
-    sheetOptions = gui.addFolder('Sheet'),
-    renderOptions = gui.addFolder('Render')
-
-  sheetOptions.addColor(SHEET_OPTIONS, 'color').onChange(
-    newColor => mesh.material.color.set(newColor)
-  )
-  sheetOptions.open()
-
-  renderOptions.addColor(RENDER_OPTIONS, 'backgroundColor').onChange(
-    newColor => scene.background.set(newColor)
-  )
-  renderOptions.add(camera.position, 'z').min(5).max(30).step(5).listen()
-  // renderOptions.add(camera, 'fov').min(0).max(125).step(25).listen()
-  renderOptions.add(mesh.material, 'wireframe').listen()
-  renderOptions.open()
-}
-
-
-//
-// window book-keeping
-//
-
-
-function onWindowLoad (_unusedEvent = null) {
-  initScene(RENDER_OPTIONS);
-  animateScene();
-  onWindowResize();
-  initDatGUI(mesh, camera)
-
-  gsap.to(mesh.rotation, {
-    delay: 1, duration: 2, x: 2, y:-2, z:1, repeat: -1, yoyo: true
-  });
-}
-
-
-function onWindowResize (_unusedEvent = null) {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-
-//
-// bind events to handlers
-//
-
-
-window.addEventListener('load', onWindowLoad);
-window.addEventListener('resize', onWindowResize);
