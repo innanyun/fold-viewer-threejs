41185236e6a9e29aed219677d5b442f7fa652a6c
Innan Yun <innan.yun@gmail.com>
2021-07-04 10:25:55 +0800

    minimize exported names

--- src/sheet_test.js
+++ src/sheet_test.js
@@ -1,18 +1,10 @@
 import { strict as assert } from 'assert'
 
-import {
-  _squareSheetVertexLocations,
-  _squareSheetVertexPositions
-} from './sheet.js'
+import { squareSheetVertexPositions } from './sheet.js'
 
 
 const
-  vertexLocations = _squareSheetVertexLocations(2.0),
-  vertexPositions = _squareSheetVertexPositions()
-
-
-assert(vertexLocations.length == 4)
-console.log('vertex planar coords:', vertexLocations)
+  vertexPositions = squareSheetVertexPositions(5)
 
 assert(vertexPositions.length == 4)
 console.log('vertex spatial coords:', vertexPositions)
