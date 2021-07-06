f2b5672f8132df4cfb04d0cac1b30f570c99a050
Innan Yun <innan.yun@gmail.com>
2021-07-06 09:21:48 +0800

    type casting to fix tsc compilation errors

--- src/debug.ts
+++ src/debug.ts
@@ -13,12 +13,14 @@ export function initDatGUI (
     renderOptions: dat.GUI = gui.addFolder('Render')
 
   sheetOptions.addColor(SHEET_OPTIONS, 'color').onChange(
-    newColor => mesh.material.color.set(newColor)
+    newColor => (mesh.material as THREE.MeshStandardMaterial).color.set(
+      newColor
+    )
   )
   sheetOptions.open()
 
   renderOptions.addColor(RENDER_OPTIONS, 'backgroundColor').onChange(
-    newColor => scene?.background?.set(newColor)
+    newColor => (scene.background as THREE.Color).set(newColor)
   )
   renderOptions.add(camera.position, 'z').min(5).max(30).step(5).listen()
   // renderOptions.add(camera, 'fov').min(0).max(125).step(25).listen()
