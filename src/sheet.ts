125abb04bbbcb01c608285ab6caf0b85eadd800a
Innan Yun <innan.yun@gmail.com>
2021-07-06 13:55:01 +0800

    add vertex locations field to `Sheet`

--- src/sheet.ts
+++ src/sheet.ts
@@ -1,59 +1,36 @@
 import { VertexId, Vector2Coord, Vector3Coord } from './types'
 
 
-//
-// 2D vertex coords of square sheet centered at the origin (0, 0)
-//
-function _squareSheetVertexLocations (size: number): Array<Vector2Coord> {
-  return [
-    [-0.5, -0.5],
-    [ 0.5, -0.5],
-    [ 0.5,  0.5],
-    [-0.5,  0.5],
-  ].map(([u, v]) => [size * u, size * v])
-}
-
-
-function _squareSheetVertexIndices (): Array<VertexId> {
-  return [0, 1, 2, 3]
-}
-
-
-//
-// 3D vertex coords of square sheet from 2D counterparts
-//
-function squareSheetVertexPositions (size: number = 1.0)
-: Array<Vector3Coord>
-{
-  return _squareSheetVertexLocations(size).map(([u, v]) => [u, v, 0.0])
-}
-
-
 interface Sheet {
+
+  vertexLocations: () => Array<Vector2Coord>
 
   vertexPositions: () => Array<Vector3Coord>
 
   faceVertexIndices: () => Array<Array<VertexId>>
+
 }
 
 
 class SquareSheet implements Sheet {
 
+  #_vertexLocations: Array<Vector2Coord>
   #_vertexPositions: Array<Vector3Coord>
-
   #_faceVertexIndices: Array<Array<VertexId>>
 
-  vertexPositions(): Array<Vector3Coord> {
-    return this.#_vertexPositions
-  }
-
-  faceVertexIndices(): Array<Array<VertexId>> {
-    return this.#_faceVertexIndices
-  }
+  vertexLocations (): Array<Vector2Coord> { return this.#_vertexLocations }
+  vertexPositions (): Array<Vector3Coord> { return this.#_vertexPositions }
+  faceVertexIndices (): Array<Array<VertexId>> { return this.#_faceVertexIndices}
 
   constructor(size: number = 1.0) {
-    this.#_vertexPositions = squareSheetVertexPositions(size),
-    this.#_faceVertexIndices = [_squareSheetVertexIndices()]
+    this.#_vertexLocations = [
+      [-0.5, -0.5],
+      [ 0.5, -0.5],
+      [ 0.5,  0.5],
+      [-0.5,  0.5],
+    ].map(([u, v]) => [size * u, size * v])
+    this.#_vertexPositions = this.#_vertexLocations.map(([u, v]) => [u, v, 0.0])
+    this.#_faceVertexIndices = [[0, 1, 2, 3]]
   }
 
 }
@@ -61,6 +38,5 @@ class SquareSheet implements Sheet {
 
 
 export {
-  Sheet, SquareSheet,
-  squareSheetVertexPositions
+  Sheet, SquareSheet
 }
