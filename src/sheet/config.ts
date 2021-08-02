b1e36284ba5f258e42381f06aee8dc0ac5602e52
Innan Yun <innan.yun@gmail.com>
2021-08-03 02:01:53 +0800

    split config for sheet and view

--- src/sheet/config.ts
+++ src/sheet/config.ts
@@ -1,14 +1,9 @@
-interface SheetOptions {
+export interface SheetOptions {
   size: number
   color: string | number
 }
 
-const SHEET_OPTIONS: SheetOptions = {
-    size: 2,
-    color: 0x00ff00
-  }
-
-
-export {
-  SheetOptions, SHEET_OPTIONS
+export const SHEET_OPTIONS: SheetOptions = {
+  size: 2,
+  color: 0x00ff00
 }
---
+++ src/view/config.ts
@@ -0,0 +1,17 @@
+export interface ViewOptions {
+  dom: HTMLElement
+  backgroundColor: string | number
+  fov: number
+  near: number
+  far: number
+}
+
+export const VIEW_OPTIONS: ViewOptions = {
+  dom: document.getElementById('container')!,
+  // Scene
+  backgroundColor: '#ceedce',
+  // Camera
+  fov: 75,    // field of view
+  near: 0.1,  // near plane of viewing frustum
+  far: 1000   // far plane of viewing frustum
+}
