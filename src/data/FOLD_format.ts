359fc82c01ef5c8a734913e7aa30e3412e5e91e9
Innan Yun <innan.yun@gmail.com>
2021-08-07 23:27:08 +0800

    add type for FOLD file format

---
+++ src/data/FOLD_format.ts
@@ -0,0 +1,69 @@
+import { Vector2Coord, Vector3Coord, VertexId, EdgeId, FaceId } from 'sheet/types';
+
+
+type FOLD_format = FOLD_file_metadata & FOLD_frame_metadata & FOLD_geometric_data;
+
+
+type CustomProperty = string;
+
+
+interface FOLD_file_metadata {
+  file_spec: number;
+  file_creator: string;
+  file_author: string;
+  file_title: string;
+  file_description: string;
+  file_classes: Array<
+    ("singleModel" | "multiModel") | "animation" | "diagrams" | CustomProperty
+  >;
+  file_frames: Array<FOLD_frame_data>;
+}
+
+
+type FOLD_frame_data = FOLD_frame_metadata & FOLD_geometric_data;
+
+
+interface FOLD_frame_metadata {
+  frame_author?: string;
+  frame_title: string;
+  frame_description: string;
+  frame_classes:
+    | "creasePattern"
+    | "foldedForm"
+    | "graph"
+    | "linkage"
+    | CustomProperty;
+  frame_attributes:
+    | ("2D" | "3D")
+    | "abstract"
+    | ("manifold" | "nonManifold")
+    | ("orientable" | "nonOrientable")
+    | ("selfTouching" | "nonSelfTouching")
+    | ("selfIntersecting" | "nonSelfIntersecting")
+    | CustomProperty;
+  frame_unit: "unit" | "in" | "pt" | "m" | "cm" | "mm" | "um" | "nm";
+}
+
+
+interface FOLD_geometric_data {
+  vertices_coords: Array<Vector2Coord | Vector3Coord>;
+  vertices_vertices: Array<Array<VertexId>>;
+  vertices_faces: Array<Array<FaceId>>;
+
+  edges_vertices: Array<[VertexId, VertexId]>;
+  edges_faces: Array<[FaceId, FaceId] | FaceId>;
+  edges_assignment: Array<'B' | 'M' | 'V' | 'F' | 'U'>;
+  edges_foldAngle: Array<number>;
+  edges_length: Array<number>;
+
+  faces_vertices: Array<Array<VertexId>>
+  faces_edges: Array<Array<EdgeId>>
+
+  faceOrders: Array<[FaceId, FaceId, (1 | -1 | 0)]>;
+  edgeOrders: Array<[EdgeId, EdgeId, (1 | -1 | 0)]>;
+}
+
+
+export {
+  FOLD_format
+}
--- src/sheet/types.ts
+++ src/sheet/types.ts
@@ -2,12 +2,13 @@
 // type aliases
 //
 type VertexId = number
+type EdgeId = number
+type FaceId = number
 type Vector2Coord = [number, number]
 type Vector3Coord = [number, number, number]
 
 
 export {
-  VertexId,
-  Vector2Coord,
-  Vector3Coord
+  VertexId, EdgeId, FaceId,
+  Vector2Coord, Vector3Coord
 }
