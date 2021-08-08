dbc8afe68fa4c8ed032c3abb9359a215eb471f0a
Innan Yun <innan.yun@gmail.com>
2021-08-08 08:44:40 +0800

    add example .FOLD files (brought from the FOLD project)

--- src/data/FOLD_reader_test.ts
+++ src/data/FOLD_reader_test.ts
@@ -5,11 +5,17 @@
  *  node build/test.js <FOLD_file_path>
  */
 
+import * as fs from "fs";
+
 import { FOLD_format } from 'data/FOLD_format';
-import * as fs from 'fs'
+
+import imported from './test-data/simple.json';
+
 
 const
   last = (arr: string[]): string => arr[arr?.length - 1],
-  f: FOLD_format = JSON.parse(fs.readFileSync(last(process.argv)).toString());
+  f: FOLD_format = JSON.parse(fs.readFileSync(last(process.argv)).toString()),
+  g: FOLD_format = imported;
 
 console.log(f);
+console.log(g);
