3f524c5523973ce774b030fb3b43eab7b9bbf848
Innan Yun <innan.yun@gmail.com>
2021-08-25 01:40:11 +0800

    more modularized dat.GUI

--- src/system/debug.ts
+++ src/system/debug.ts
@@ -6,7 +6,7 @@ import { VIEW_OPTIONS } from 'view/config'
 
 
 export function initDatGUI(
-  scene: THREE.Scene, mesh: THREE.Object3D, camera: THREE.Camera
+  scene: THREE.Scene, sheetMesh: THREE.Object3D, camera: THREE.Camera
 ): void
 {
   let
@@ -14,26 +14,43 @@ export function initDatGUI(
     sheetOptions = gui.addFolder('Sheet'),
     renderOptions = gui.addFolder('Render')
 
-  sheetOptions.addColor(SHEET_OPTIONS, 'color').onChange(
-    (newColor: string | THREE.Color) => updateSheetColor(mesh)(newColor)
-  )
-  sheetOptions.open()
-
-  renderOptions.addColor(VIEW_OPTIONS, 'backgroundColor').onChange(
-    newColor => (scene.background as THREE.Color).set(newColor)
-  )
-  renderOptions.add(camera.position, 'z').min(5).max(30).step(5).listen()
-  renderOptions.add(camera, 'fov').min(0).max(125).step(25).listen()
-  // renderOptions.add(mesh.material, 'wireframe').listen()
-  renderOptions.open()
+  _setSheetOptions(sheetOptions, sheetMesh)
+  _setRenderingOptions(renderOptions, scene, camera)
 }
 
 
-const updateSheetColor = (s: THREE.Object3D) =>
-  (newColor: string | THREE.Color): void => {
-    s.traverse((mesh: THREE.Object3D) => {
-      if (mesh instanceof THREE.Mesh) {
-        mesh.material.color.set(newColor)
-      }
-    })
+function _setSheetOptions(
+  sheetOptions: dat.GUI, sheetMesh: THREE.Object3D
+): void {
+
+  function _updateSheetColor(sheetMesh: THREE.Object3D): Function {
+    return function (newColor: string | THREE.Color): void {
+      sheetMesh.traverse((mesh: THREE.Object3D) => {
+        if (mesh instanceof THREE.Mesh) {
+          mesh.material.color.set(newColor)
+        }
+      })
+    }
   }
+
+  sheetOptions
+    .addColor(SHEET_OPTIONS, 'color')
+    .onChange(newColor => _updateSheetColor(sheetMesh)(newColor))
+
+  sheetOptions.open()
+}
+
+
+function _setRenderingOptions(
+  renderOptions: dat.GUI, scene: THREE.Scene, camera: THREE.Camera
+): void {
+
+  renderOptions
+    .addColor(VIEW_OPTIONS, 'backgroundColor')
+    .onChange(newColor => (scene.background as THREE.Color).set(newColor))
+  renderOptions.add(camera.position, 'z').min(5).max(30).step(5).listen()
+  renderOptions.add(camera, 'fov').min(0).max(125).step(25).listen()
+  // renderingOptionsGui.add(mesh.material, 'wireframe').listen()
+
+  renderOptions.open()
+}
